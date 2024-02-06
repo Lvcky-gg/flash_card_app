375a55e3fcbe5dfa0e3f30d743a083557f6e2a7d
John O'Donnell <matadorcrab@gmail.com>
2024-02-06 18:55:31 +0000

    added an exit case

--- main.c
+++ main.c
@@ -1,23 +1,25 @@
 #include "./make_directory.h"
 #include "./scan.h"
 #include "./search_directories.h"
-#include <stdio.h>
+#include <stdlib.h>
 int main() {
   int action = 0;
   char *build_directory = "./LvckyCards";
   check_if_directory(build_directory);
-
   do {
-    action = welcome();
-  } while (action != 1 && action != 2);
+    do {
+      action = welcome();
+    } while (action != 1 && action != 2 && action != 3);
 
-  switch (action) {
-  case 1:
-    make_path(build_directory);
-    break;
-  case 2:
-    break;
-  }
+    switch (action) {
+    case 1:
+      make_path(build_directory);
+      break;
+    case 2:
+      break;
+    }
+    system("clear");
+  } while (action != 3);
   // printf("%d", action);
   return 0;
 }
--- scan.c
+++ scan.c
@@ -3,8 +3,9 @@
 int welcome() {
   int val;
   printf("Welcome to LvckyCards! What would you like to do?\n");
-  printf("1: create a deck\n");
-  printf("2: open a deck\n");
+  printf("   1: create a deck\n");
+  printf("   2: open a deck\n");
+  printf("   3: exit\n");
   scanf("%d", &val);
 
   return val;
