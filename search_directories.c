25e61736167c93cab9bc21bd468002acb6783d36
John O'Donnell <matadorcrab@gmail.com>
2024-02-06 01:47:30 +0000

    directory already exists

--- search_directories.c
+++ search_directories.c
@@ -1,18 +1,29 @@
 
 
+#include <dirent.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 bool check_if_directory(char *path) {
-  int ret = 0;
-  ret = mkdir(path, 0777);
-  if (ret == 0) {
-    printf("Directory created successfully\n");
-    return true;
-  } else {
-    printf("Unable to create directory %s\n", path);
+
+  DIR *dir = opendir(path);
+  if (dir) {
+    /* Directory exists. */
+    closedir(dir);
+    printf("Directory already exists!");
+    return false;
+  } else if (ENOENT == errno) {
+    int ret = 0;
+    ret = mkdir(path, 0777);
+    if (ret == 0) {
+      printf("Directory created successfully\n");
+      return true;
+    } else {
+      printf("Unable to create directory %s\n", path);
+    }
   }
   return false;
 }
