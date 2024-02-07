90a3507935b70566db59482e89bf53f0fe3006a9
John O'Donnell <matadorcrab@gmail.com>
2024-02-07 04:51:11 +0000

    error handling

--- scan.c
+++ scan.c
@@ -9,6 +9,9 @@ int welcome() {
   printf("   2: open a deck\n");
   printf("   3: exit\n");
   scanf("%d", &val);
+  if (val == 1 || val == 2 || val == 3) {
+    return val;
+  }
 
-  return val;
+  return 0;
 }
